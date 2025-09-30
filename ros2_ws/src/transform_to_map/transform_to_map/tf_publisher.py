7748c1b879ec38b77f505bc82a9baa10090e1cd1
Dennis <mcde1011@h-ka.de>
2025-09-30 16:28:17 +0200

    adjust camera-link transform in config-file

--- ros2_ws/src/transform_to_map/transform_to_map/tf_publisher.py
+++ ros2_ws/src/transform_to_map/transform_to_map/tf_publisher.py
@@ -7,33 +7,45 @@ import tf_transformations
 class StaticTFPublisher(Node):
     def __init__(self):
         super().__init__('static_tf_publisher')
-        
-        # read params.yaml
-        self.declare_parameter("camera_frame", "camera_link")
-        self.camera_frame = self.get_parameter("camera_frame").get_parameter_value().string_value
-        self.declare_parameter("base_frame", "hkaspot/base_link")
-        self.base_frame = self.get_parameter("base_frame").get_parameter_value().string_value
-        
-        self.broadcaster = StaticTransformBroadcaster(self)
-        self.publish_static_transform()
 
-    def publish_static_transform(self):
+        # set default parameter
+        self.declare_parameter("camera_frame", "camera_link")
+        self.declare_parameter("base_frame", "hkaspot/base_link")
+        self.declare_parameter("transform.translation", [-0.117, 0.0, 0.111])
+        self.declare_parameter("transform.euler", [0.0, 0.0, 0.0])  # roll, pitch, yaw in Radians
+
+        # read config file
+        self.camera_frame = self.get_parameter("camera_frame").value
+        self.base_frame = self.get_parameter("base_frame").value
+        self.transform_translation = self.get_parameter("transform.translation").value
+        self.transform_euler = self.get_parameter("transform.euler").value
+
+        q = tf_transformations.quaternion_from_euler(
+            float(self.transform_euler[0]),
+            float(self.transform_euler[1]),
+            float(self.transform_euler[2]),
+        )
+
+        self.broadcaster = StaticTransformBroadcaster(self)
+        self.publish_static_transform([float(q[0]), float(q[1]), float(q[2]), float(q[3])])
+
+    def publish_static_transform(self, quat):
         t = TransformStamped()
         t.header.stamp = rclpy.time.Time().to_msg()
         t.header.frame_id = self.base_frame
         t.child_frame_id = self.camera_frame
-        t.transform.translation.x = -0.117
-        t.transform.translation.y = 0.0
-        t.transform.translation.z = 0.111
 
-        # Quaternion aus Euler-Winkeln (hier: keine Rotation)
-        quat = tf_transformations.quaternion_from_euler(0, 0, 0)
+        t.transform.translation.x = float(self.transform_translation[0])
+        t.transform.translation.y = float(self.transform_translation[1])
+        t.transform.translation.z = float(self.transform_translation[2])
+
         t.transform.rotation.x = quat[0]
         t.transform.rotation.y = quat[1]
         t.transform.rotation.z = quat[2]
         t.transform.rotation.w = quat[3]
 
         self.broadcaster.sendTransform(t)
+        self.get_logger().info(f"Static transform published: {self.base_frame} -> {self.camera_frame}")
 
 def main():
     rclpy.init()
